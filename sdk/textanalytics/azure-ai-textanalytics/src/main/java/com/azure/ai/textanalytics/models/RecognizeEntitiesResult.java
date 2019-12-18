fd1091efb36e7e809da1378de05f9c27ce9644b8
Sameeksha Vaity <savaity@microsoft.com>
2019-12-18 14:19:49 -0800

    adding mapping updates

--- sdk/textanalytics/azure-ai-textanalytics/src/main/java/com/azure/ai/textanalytics/models/RecognizeEntitiesResult.java
+++ sdk/textanalytics/azure-ai-textanalytics/src/main/java/com/azure/ai/textanalytics/models/RecognizeEntitiesResult.java
@@ -8,21 +8,21 @@ import com.azure.core.annotation.Immutable;
 import java.util.List;
 
 /**
- * The NamedEntityResult model.
+ * The RecognizeEntitiesResult model.
  */
 @Immutable
-public final class NamedEntityResult extends DocumentResult {
+public final class RecognizeEntitiesResult extends DocumentResult {
     private final List<NamedEntity> namedEntities;
 
     /**
-     * Creates a {@code NamedEntityResult} model that describes recognized entities result
+     * Creates a {@code RecognizeEntitiesResult} model that describes recognized entities result
      *
      * @param id unique, non-empty document identifier
      * @param textDocumentStatistics text document statistics
      * @param error the document error
      * @param namedEntities a list of {@link NamedEntity}
      */
-    public NamedEntityResult(String id, TextDocumentStatistics textDocumentStatistics, Error error,
+    public RecognizeEntitiesResult(String id, TextDocumentStatistics textDocumentStatistics, Error error,
                              List<NamedEntity> namedEntities) {
         super(id, textDocumentStatistics, error);
         this.namedEntities = namedEntities;
