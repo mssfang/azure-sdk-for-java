3b5aa2bcfadbf0bb387a28e15ded34add3f0ba3e
Sameeksha Vaity <savaity@microsoft.com>
2020-09-16 17:21:48 -0700

    add support for composed model

---
+++ sdk/formrecognizer/azure-ai-formrecognizer/src/main/java/com/azure/ai/formrecognizer/training/models/CustomModelProperties.java
@@ -0,0 +1,9 @@
+package com.azure.ai.formrecognizer.training.models;
+
+public class CustomModelProperties {
+    private boolean compositeModel;
+
+    public boolean isCompositeModel() {
+        return this.compositeModel;
+    }
+}
